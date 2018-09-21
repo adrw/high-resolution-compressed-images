19e65fb663d9e0f9b9051e057ff866abbcd350e4
Andrew Paradi <account+github@adrw.ch>
2018-09-21 14:27:51 -0400

    Increased compression rate with reduced --floyd and removed --posterize option

--- optimize.sh
+++ optimize.sh
@@ -11,13 +11,12 @@ usage() {
 
   Files are then copies to ../img
   
-  Learn more http://hrci.adrw.ch/
+  Learn more http://g.adrw.ch/high-resolution-compressed-images
 EOF
   exit 1
 }
 
-dir=$([[ $# -ne 1 ]] && echo "$(pwd)" || echo "$1")
-[[ -d $dir ]] || usage
-cd $dir
-find . -name "*.png" -print0 | xargs -0 -P8 -L1 pngquant --ext c.png --speed 1 --quality=0-10 --posterize 4 --strip --floyd=0.05 --force 4
+dir=$([ $# -ne 1 ] && pwd || echo "$1")
+cd $dir || usage
+find . -name "*.png" -print0 | xargs -0 -P8 -L1 pngquant --ext c.png --speed 1 --quality=0-1 --strip --floyd=0.02 --force 4
 mv *c.png ../img
